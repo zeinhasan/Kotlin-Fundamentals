208d07faa576b26ee079cd7835868945ab4517ca
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-04-29 10:19:14 +0700

    Add materials

---
+++ Intoduction-to-Kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // introduction to kotlin
+    // Prints "Hello World!" to the terminal window.
+    println("Hello World!")
+    println("This is my first kotlin program")
+    println("I am learning kotlin")
+}
