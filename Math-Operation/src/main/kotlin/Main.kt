ecd8957203f61fb1311bb9c865c5a88ffed5e5d5
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-04-29 16:49:19 +0700

    Add Math Operation

---
+++ Math-Operation/src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun main() {
+    // Math operations in Kotlin
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ (increment)
+    // -- (decrement)
+
+    // Addition
+    val addition:Int = 1 + 1
+    println("1 + 1 = $addition")
+
+    // Subtraction
+    val subtraction:Int = 2 - 1
+    println("2 - 1 = $subtraction")
+
+    // Multiplication
+    val multiplication:Int = 2 * 2
+    println("2 * 2 = $multiplication")
+
+    // Division
+    val division = 4.0 / 2.0
+    println("4 / 2 = $division")
+
+    // Modulus
+    val modulus:Int = 5 % 2
+    println("5 % 2 = $modulus")
+
+    // Increment
+    var increment:Int = 1
+    increment++
+    println("1++ = $increment")
+
+    // Decrement
+    var decrement:Int = 2
+    decrement--
+    println("2-- = $decrement")
+
+}
