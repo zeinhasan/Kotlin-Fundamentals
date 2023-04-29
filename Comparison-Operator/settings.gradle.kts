8fe75d0f549fa96d0874c42bce4783ad4f09d80a
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-04-29 16:58:11 +0700

    Add Comparison Operator

---
+++ Comparison-Operator/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Comparison-Operator"
+
---
+++ Comparison-Operator/src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main() {
+    // Comparison operators
+    // >, <, >=, <=, ==, !=
+    // > greater than
+    // < less than
+    // >= greater than or equal to
+    // <= less than or equal to
+    // == equal to
+    // != not equal to
+
+    val a = 5
+    val b = 3
+    // a > b = true
+    println(a > b)
+
+    // a < b = false
+    println(a < b)
+
+    // a >= b = true
+    println(a >= b)
+
+    // a <= b = false
+    println(a <= b)
+
+    // a == b = false
+    println(a == b)
+
+    // a != b = true
+    println(a != b)
+
+}
