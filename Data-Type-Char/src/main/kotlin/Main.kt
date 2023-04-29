d5a656eec83a93c5c4751c0bd7b639c26d374183
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-04-29 10:59:26 +0700

    Add Data Type Char

---
+++ Data-Type-Char/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // Char type
+    var e: Char = 'E' // Char type must be in single quote
+    var f: Char = 'F' // Char type must be in single quote
+    var g: Char = 'G'
+    var h: Char = 'H'
+    // print
+    print(e) // print Char type variable
+    print(f)
+    print(g)
+    print(h)
+
+}
